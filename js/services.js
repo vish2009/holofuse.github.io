41463c52ac9547c282605ddaf0d6f9ce5926acce
vish2009 <59371997+vish2009@users.noreply.github.com>
2021-09-07 18:12:49 +0530

    New changes

--- js/services.js
+++ js/services.js
@@ -1,9 +1,12 @@
 (function(){
       $('#services-list > li > a').click(function(){
+        $('#services-list > li > a').css('color','blue')
+        $(this).css('color','red');
         const tabname = $(this).attr("href");
         
         $('#services-display > div:visible').fadeOut(200,function(){
           $(tabname).fadeIn(200);
         })
+
       });
 }());
